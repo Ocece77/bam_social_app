6ad382e4bb4f5b27c3aa7fbc05f3c5da5cb75db3
Ocece77 <oceanekasindupro@gmail.com>
2024-07-15 11:33:06 +0200

    configuration of the db

---
+++ api/app.js
@@ -0,0 +1,32 @@
+import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
+import path from 'path'
+import helmet from 'helmet'
+import { fileURLToPath } from "url"
+import cookieParser from 'cookie-parser'
+import mongoose from "mongoose"
+
+dotenv.config({path : '../.env'})
+const app = express()
+const PORT = process.env.PORT || 3000
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+app.use(cors())
+app.use(helmet())
+app.use(cookieParser())
+app.use(express.json())
+app.use(express.static(path.join(__dirname , "../client/dist")))
+
+mongoose.connect( process.env.MONGO)
+.then(()=> console.log("The db is connected"))
+
+app.listen(PORT,()=>{
+  console.log("running on port" , PORT)
+})
+
+
+
+
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig, loadEnv ,  ConfigEnv, UserConfig } from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+
+export default ({ command  , mode }: ConfigEnv): UserConfig =>  {
+  const env = loadEnv(mode, process.cwd());
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+     /* proxy: {
+        '/api': {
+          //target: env.VITE_API_KEY,
+          changeOrigin: true,
+          secure: true,
+        },
+      },*/
+    },
+    define: {
+      'process.env': JSON.stringify(env),
+    },
+  });
+};
